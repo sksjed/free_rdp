1550c760fab276586ca24560a5f3bb391a41d7df
unknown <3676946@163.com>
2021-05-29 12:59:27 +0800

    init

---
+++ start.bat
@@ -0,0 +1,20 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+net user %1 %2 /add >nul
+net localgroup administrators %1 /add >nul
+net user %1 /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant %1:F >nul
+ICACLS C:\Windows\installer /grant %1:F >nul
+echo Success!
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+echo Username: %1
+echo Password: %2
+echo You can login now.
+ping -n 10 127.0.0.1 >nul
